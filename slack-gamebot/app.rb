23dd69fdac93a630c59205305cbe9ab7c3be5788
Henry Darnell <hjdarnel@users.noreply.github.com>
2017-09-28 02:21:43 -0500

    Remove "deactivated team" feature

--- slack-gamebot/app.rb
+++ slack-gamebot/app.rb
@@ -8,34 +8,9 @@ EOS
     def prepare!
       super
       update_unbalanced_teams!
-      deactivate_dead_teams!
     end
 
     private
-
-    def deactivate_dead_teams!
-      Team.active.each do |team|
-        next if team.premium?
-        next unless team.dead?
-        begin
-          team.deactivate!
-          team.inform! DEAD_MESSAGE, 'dead'
-        rescue StandardError => e
-          logger.warn "Error informing team #{team}, #{e.message}."
-        end
-      end
-    end
-
-    def nudge_sleeping_teams!
-      Team.active.each do |team|
-        next unless team.nudge?
-        begin
-          team.nudge!
-        rescue StandardError => e
-          logger.warn "Error nudging team #{team}, #{e.message}."
-        end
-      end
-    end
 
     def update_unbalanced_teams!
       Team.where(unbalanced: nil).update_all(unbalanced: false)
